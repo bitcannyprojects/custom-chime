a743b2ef30ba338b5ea5cb221238bf234790ec81
ashutoshbitcanny <ashutosh.kumar@bitcanny.com>
2021-04-23 09:42:37 +0530

    update

--- dist/MeetingMessagePopUp/MeetingMessagePopUp.js
+++ dist/MeetingMessagePopUp/MeetingMessagePopUp.js
@@ -19,12 +19,6 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-// import { useSelector, useDispatch } from "react-redux";
-// import axios from "axios";
-// import keys from "../config/keys";
-// import { UPDATE_MESSAGE_STATE } from "../redux/types";
-// import { readMessageAction } from "../redux/actions";
-// import ScrollToBottom from "react-scroll-to-bottom";
 function MeetingMessagePopUp(_ref) {
   var sessionId = _ref.sessionId,
       sendMessage = _ref.sendMessage,
